7910db4ec20ea6a6a38225eaadeaa8f7b1ddb51b
yago <yago.k2@googlemail.com>
2022-01-04 15:45:50 +0000

    dncnumber test

---
+++ test/dncnumber_test.js
@@ -0,0 +1,79 @@
+import assert from "assert"
+import DNCNumber from "../src/types/DNCNumber.js"
+
+describe("DNCNumber",()=>{
+    describe("calcBits",()=>{
+        it("decimal numbers",()=>{
+            assert.equal(DNCNumber.calcBits(22),8)
+            assert.equal(DNCNumber.calcBits(4097),16)
+        })
+        // it("decimal strings",()=>{
+        //     assert.equal(DNCNumber.calcBits("22"),8)
+        // })
+
+    })
+    describe("parse",()=>{
+        it("decimal numbers",()=>{
+            assert.equal(DNCNumber.parse("0").val,0)
+            assert.equal(DNCNumber.parse("1").val,1)
+        })
+        it("hexadecimal numbers",()=>{
+            assert.equal(DNCNumber.parse("$10").val,16)
+            assert.equal(DNCNumber.parse("$ff").val,255)
+            assert.equal(DNCNumber.parse("$1000").val,4096)
+
+        })
+        it("binary numbers",()=>{
+            assert.equal(DNCNumber.parse("%0").val,0)
+            assert.equal(DNCNumber.parse("%1").val,1)
+        })
+    })
+
+    describe("parse dnc",()=>{
+        it("decimal numbers",()=>{
+            assert.equal(DNCNumber.parse("?").val,0)
+        })
+        it("hexadecimal numbers",()=>{
+            assert.equal(DNCNumber.parse("$1?").val,16)
+            assert.equal(DNCNumber.parse("$?f").val,15)
+            let actual=DNCNumber.parse("$1???")
+            assert.equal(actual.val,4096)
+            assert.equal(actual.dnc,4095)
+            assert.equal(DNCNumber.parse("$1??2").val,4098)
+
+        })
+        it("binary numbers",()=>{
+            assert.equal(DNCNumber.parse("%0?").val,0)
+            assert.equal(DNCNumber.parse("%?1").val,1)
+            let actual=DNCNumber.parse("%1???????")
+            assert.equal(actual.val,128)
+            assert.equal(actual.dnc,127)
+        })
+    })
+    describe("parse dnc with dncBit=1",()=>{
+        it("binary numbers",()=>{
+            DNCNumber.dncBit="1"
+            assert.equal(DNCNumber.parse("%0?").val,1)
+            assert.equal(DNCNumber.parse("%?1").val,3)
+            DNCNumber.dncBit="0"
+        })
+        it("hexadecimal numbers",()=>{
+            DNCNumber.dncNib="f"
+            assert.equal(DNCNumber.parse("$1?").val,31)
+            assert.equal(DNCNumber.parse("$?f").val,255)
+            assert.equal(DNCNumber.parse("$1???").val,8191)
+            DNCNumber.dncNib="0"
+        })
+    })
+    describe("constructor",()=>{
+        it("with two args",()=>{
+            let actual=new DNCNumber(8,100)
+            let expected={bits:8, val:100}
+            assert.equal(actual.bits,expected.bits)
+            assert.equal(actual.val,expected.val)
+        })
+    })
+})
+
+
+
