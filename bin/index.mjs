6b721ed66f059aa643a9e73754674a9ae485972f
yago <yago.k2@googlemail.com>
2022-01-12 15:44:00 +0000

    asm running on cli

--- bin/index.mjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import Assembler from "./Assembler.js"
-
-console.log("running on nodebin")
--- src/index.mjs
+++ src/index.mjs
@@ -17,10 +17,22 @@ else {
   let argv = yargs(process.argv.slice(2))
     .option("t", { alias: "ticks", default: 50, describe: "Ticks per second", type: "number" })
     .option("o", { alias: "outfile", describe: "File to output to", type: "string" })
+    .option("d", { alias: "dncfile", describe: "File with DNC", type: "string" })
+    .option("D",{alias:"define", describe: "Define labels", type: "string"})
+    .example("k2asm main.src -o main.obj","Assembling")
+    .example("k2asm main.src -DDEBUG -o main.obj","Assembling with DEBUG set (to true)")
+    .example("k2asm main.src -DMUSIC=false -o main.obj","Assembling with MUSIC=false")
     .parse()
   let asm = new Assembler(argv)
   let src = fs.readFileSync(argv._[0]).toString()
   asm.assemble(src)
-  let obj=asm.getObject()
-  fs.writeFileSync(argv.outfile,Uint8Array.from(obj))
+  let obj = asm.getObject()
+  let out = Uint8Array.from(obj)
+  let outfile = argv.outfile
+  if (!outfile) {
+    process.stdout.write(out)
+  }
+  else {
+    fs.writeFileSync(outfile, out)
+  }
 }
